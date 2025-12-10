0075336d429d34a8fd9a6ec8f8515caf11b46f30
shibikasrig <151347452+shibikasrig@users.noreply.github.com>
2025-12-10 21:22:35 +0530

    lambda_function.py

---
+++ lambda_function.py
@@ -0,0 +1,23 @@
+import json
+import boto3
+
+def lambda_handler(event, context):
+    s3 = boto3.client("s3")
+    sns = boto3.client("sns")
+
+    bucket = event["Records"][0]["s3"]["bucket"]["name"]
+    file_name = event["Records"][0]["s3"]["object"]["key"]
+
+    # Read file content
+    file_obj = s3.get_object(Bucket=bucket, Key=file_name)
+    file_content = file_obj["Body"].read().decode("utf-8")
+
+    # If log contains "ERROR", send SNS alert
+    if "ERROR" in file_content:
+        sns.publish(
+            TopicArn="arn:aws:sns:us-east-1:551507899771:intelligent-monitoring-alerts",
+            Message=f"Alert! Error detected in log file: {file_name}\n\nContent:\n{file_content}",
+            Subject="Cloud Monitoring Alert"
+        )
+
+    return {"status": "processed"}
